670efb07826abb66dc616d662f153cb38f5557ff
michpl76 <50846646+michpl76@users.noreply.github.com>
2020-10-24 22:57:31 +0500

    HW 3 - add meals to initDB.sql and populateDB.sql, implement JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +10,19 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id, date_time)
+VALUES ('завтрак',  250, 100000, timestamp '2020-10-23 07:30'),
+       ('обед',     450, 100000, timestamp '2020-10-23 12:30'),
+       ('ужин',     550, 100000, timestamp '2020-10-23 18:30'),
+       ('завтрак',  300, 100000, timestamp '2020-10-24 08:15'),
+       ('обед',     500, 100000, timestamp '2020-10-24 13:15'),
+       ('ужин',     550, 100000, timestamp '2020-10-24 19:25'),
+
+       ('завтрак',  250, 100001, timestamp '2020-10-23 07:45'),
+       ('обед',     450, 100001, timestamp '2020-10-23 12:45'),
+       ('ужин',     550, 100001, timestamp '2020-10-23 18:45'),
+       ('завтрак',  300, 100001, timestamp '2020-10-24 08:10'),
+       ('обед',     500, 100001, timestamp '2020-10-24 13:10'),
+       ('ужин',     550, 100001, timestamp '2020-10-24 19:10')
+;
